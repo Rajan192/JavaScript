0ffacb287148571a1c5d2d68c21613f69e6a625b
Rajan192 <49790302+Rajan192@users.noreply.github.com>
2021-02-16 12:44:13 +0530

    Add files via upload

---
+++ FunctionPractice.js
@@ -0,0 +1,23 @@
+// function a()
+// {
+//     console.log("Welcome to the function of javaScript");
+
+// }
+// a();
+
+// function add(num1,num2)
+// {
+//     console.log("Sum of Num1 and num2 ",num1+num2);
+//     return num1*num2;
+    
+// }
+// mult=add(2,3);
+// console.log(mult);
+// var func=function func()
+// {
+//     console.log(func==func);
+// }
+// func();
+
+
+
